eac930a186ce2707905d4684b0ebd449c516c5a2
qiuyuqin <qiuyuqin@pku.edu.cn>
2015-11-16 23:02:26 +0800

    homework_7th

--- homework_7th/CountWord.java
+++ homework_7th/CountWord.java
@@ -12,6 +12,9 @@ public class CountWord {
 			//打开交集词汇表
 			FileWriter wordIntersectionout= new FileWriter("C:/Users/qiuyu/Desktop/java/wordIntersection.txt");
 			FileReader wordIntersectionin= new FileReader("C:/Users/qiuyu/Desktop/java/wordIntersection.txt");
+			//打开统计文件
+			FileWriter wordcountfileout= new FileWriter("C:/Users/qiuyu/Desktop/java/wordcountfile.txt");
+			FileReader wordcountfilein= new FileReader("C:/Users/qiuyu/Desktop/java/wordcountfile.txt");
 			//定义bufferreader方法
 			BufferedReader FirstRead = new BufferedReader(wordFirst);
 			BufferedReader SecondRead = new BufferedReader(wordSecond);
@@ -19,11 +22,18 @@ public class CountWord {
 			BufferedWriter TotalWriter = new BufferedWriter(wordtotalout);
 			BufferedReader IntersectionRead = new BufferedReader(wordIntersectionin);
 			BufferedWriter IntersectionWriter = new BufferedWriter(wordIntersectionout);
+			BufferedReader countRead = new BufferedReader(wordcountfilein);
+			BufferedWriter countWriter = new BufferedWriter(wordcountfileout);
 			//将文件读取的内容存入动态数组中
 			ArrayList FirstAarry =new ArrayList();
 			ArrayList SecondAarry =new ArrayList();
 			ArrayList TotalAarry =new ArrayList();
+			ArrayList IntersectionAarry =new ArrayList();
 			String lineStr=null;
+			//定义变量：单词属于文件1且不属于文件2中的单词数量
+			int count1not2 =0;
+			//定义变量：单词属于文件2且不属于文件1中的单词数量
+			int count2not1=0;
 			//将文件1和文件2中的单词都存入动态数组中
 			while((lineStr=FirstRead.readLine())!= null){
 				String[] splittedStr = lineStr.split("[\\s,.;。，； ]");
@@ -53,19 +63,35 @@ public class CountWord {
 							if(!TotalAarry.contains(str.trim())){
 								TotalAarry.add(str.trim());
 								TotalWriter.write(str+" ");
+								count2not1++;
 							}else{
 								//如果文件2中的单词在文件1中已经存在，则放入交集词汇表
+								IntersectionAarry.add(str.trim());
 								IntersectionWriter.write(str+" ");
 							}
 						}
 					}
 				}
 			}
-			System.out.println("!!"+FirstAarry.size());
-			System.out.println("!!"+SecondAarry.size());
+			double proportion2not1 =(double)(Math.round(count2not1*10000/SecondAarry.size())/10000.00);
+			double proportion1not2 =(double)(Math.round((TotalAarry.size()-count2not1-IntersectionAarry.size())*10000/FirstAarry.size())/10000.00);
+			System.out.println("文件1中的单词总数："+FirstAarry.size());
+			System.out.println("文件2中的单词总数："+SecondAarry.size());
+			System.out.println("总共单词数："+TotalAarry.size());
+			//System.out.println("count2not1："+count2not1);
+			//System.out.println("count1not2："+(TotalAarry.size()-count2not1-IntersectionAarry.size()));
+			System.out.println("属于文件1且不属于文件2的单词数量占文件1单词总数的比例："+proportion1not2*100+"%");
+			System.out.println("属于文件2且不属于文件1的单词数量占文件2单词总数的比例："+proportion2not1*100+"%");
+			//将统计数据写入文件
+			countWriter.write("文件1中的单词总数："+FirstAarry.size()+"\r\n");
+			countWriter.write("文件2中的单词总数："+SecondAarry.size()+"\r\n");
+			countWriter.write("总共单词数："+TotalAarry.size()+"\r\n");
+			countWriter.write("属于文件1且不属于文件2的单词数量占文件1单词总数的比例："+proportion1not2*100+"%"+"\r\n");
+			countWriter.write("属于文件2且不属于文件1的单词数量占文件2单词总数的比例："+proportion2not1*100+"%"+"\r\n");
 			//关闭缓冲区文件，使得缓冲区的数据写入文件中
 			TotalWriter.close();
 			IntersectionWriter.close();
+			countWriter.close();
 		}catch(Exception e){}
 	}
 	public static void main(String[] args){
